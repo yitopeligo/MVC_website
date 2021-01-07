bc3e80586382b904eb79083873f7f4dc1835621a
yitopeligo <57295676+yitopeligo@users.noreply.github.com>
2021-01-07 15:34:52 +0300

    Update login.php

--- login.php
+++ login.php
@@ -23,6 +23,9 @@
       }
    }
    
+   /* Da diese Anmelde-Index-Seite einfach und von der App getrennt werden sollte,
+	wird die MVC-Architektur für diese Codeseiten nicht verwendet*/
+   
    function checkUser($post_mail, $post_pw) {
 	   
 	   define('DB_SERVER', 'localhost:3306');
@@ -32,16 +35,16 @@
 	   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 	   
 	  $mymail = mysqli_real_escape_string($db, $post_mail);
-      $mypassword = mysqli_real_escape_string($db, $post_pw); 
-      
-      $sql = "SELECT id FROM users WHERE mail = '$mymail' and password = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
+	  $mypassword = mysqli_real_escape_string($db, $post_pw); 
+	  
+	  $sql = "SELECT id FROM users WHERE mail = '$mymail' and password = '$mypassword'";
+	  $result = mysqli_query($db,$sql);
+	  $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+	  $active = $row['active'];
+	  
 	  $count = mysqli_num_rows($result);
 	  
-      return $count;
+	  return $count;
 	   
    }
 ?>
