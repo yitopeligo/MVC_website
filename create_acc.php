a8d1f69a79cffbef8bc1fc496388419ed2b5602e
yitopeligo <57295676+yitopeligo@users.noreply.github.com>
2021-01-07 15:34:49 +0300

    Update create_acc.php

--- create_acc.php
+++ create_acc.php
@@ -1,4 +1,6 @@
 <?php
+	/* Da diese Anmelde-Index-Seite einfach und von der App getrennt werden sollte,
+	wird die MVC-Architektur für diese Codeseiten nicht verwendet*/
    define('DB_SERVER', 'localhost:3306');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '');
